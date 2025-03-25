27189dadfe6ee9c522ce8bb1e96ac906393f754b
Mostak <mostakahmedshahad74@gmail.com>
2025-03-26 02:52:39 +0600

    error_fixed_login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+
 import 'package:flutter/material.dart';
 import 'package:rest_api_login_registration/screens/home_screen.dart';
 
@@ -9,6 +14,48 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final String apiUrl = 'https://jsonplaceholder.typicode.com/posts'; 
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  String result = ''; 
+
+   @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _postData() async {
+    try {
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, dynamic>{
+          'name': emailController.text,
+          'email': passwordController.text,
+          // Add any other data you want to send in the body
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        // Successful POST request, handle the response here
+        final responseData = jsonDecode(response.body);
+        setState(() {
+          result = 'ID: ${responseData['id']}\nName: ${responseData['name']}\nEmail: ${responseData['email']}';
+        });
+      } else {
+        // If the server returns an error response, throw an exception
+        throw Exception('Failed to post data');
+      }
+    } catch (e) {
+      setState(() {
+        result = 'Error: $e';
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +71,9 @@ class _LoginScreenState extends State<LoginScreen> {
             children: [
               SizedBox(height: 20),
               TextField(
+                 controller: emailController,
                 decoration: InputDecoration(
+                  
                   labelText: 'Email',
                   hintText: 'Enter your email',
                 ),
@@ -32,19 +81,21 @@ class _LoginScreenState extends State<LoginScreen> {
               SizedBox(height: 20),
               TextField(
                 obscureText: true,
+                  controller: passwordController,
                 decoration: InputDecoration(
+                 
                   labelText: 'Password',
                   hintText: 'Enter your password',
                 ),
               ),
               SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-    MaterialPageRoute(builder: (context) => HomeScreen()), );
+                onPressed: 
+                    _postData,
+                 
                 
                   // Handle login logic here
-                },
+                
                 child: Text('Login'),
               ),
               SizedBox(height: 10),
